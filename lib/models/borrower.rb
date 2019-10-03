6707b701a03f97a7c07b233d4143c3cddecd0647
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-10-03 16:00:01 -0400

    repair borrower methods and manually merge

--- lib/models/borrower.rb
+++ lib/models/borrower.rb
@@ -4,98 +4,95 @@ class Borrower < ActiveRecord::Base
     @@prompt = TTY::Prompt.new
 
 
-def self.handle_new_user
-    puts "What is your name?"
-    name = gets.chomp
-        while name == "" do
-            system "clear"
-            puts "You must enter a name!".colorize(:red)
-            puts "What is your name?"
-            name = gets.chomp
-        end
-    password = @@prompt.mask("Enter a password:")
-        while password == nil do
-            system "clear"
-            puts "You must enter a password!".colorize(:red)
-            password = @@prompt.mask("Enter a password:")
-        end
-    Borrower.create(name: name, password: password, bio: "Insert bio here")
-    
-end
-
-def self.handle_returning_user
-    puts "Welcome back! What is your name?"
-    name = gets.chomp
-        while name == "" do
-            system "clear"
-            puts "You must enter a name!".colorize(:red)
-            puts "What is your name?"
-            name = gets.chomp
-        end
-    password = @@prompt.mask("Enter your password:")
-        while password == nil do
-            system "clear"
-            puts "You must enter a password!".colorize(:red)
-            password = @@prompt.mask("Enter a password:")
-        end
-    Borrower.find_by(name: name, password: password)
-end
-
-def main_menu
-    self.reload
-    system "clear"
-    puts "Welcome, " + self.name.titleize + "!"
-    @@prompt.select("What would you like to do today?".colorize(:cyan)) do |menu|
-        menu.choice "See All Books", -> {display_all_books}
-        menu.choice "See Available Books", -> {display_available_books}
-        menu.choice "See My Borrowed Books", -> {display_my_books}
-        menu.choice "Borrow a Book", -> {borrow_book}
-        menu.choice "Return A Book", -> {return_book}
-        menu.choice "Use as Lender", -> {become_lender}
-        menu.choice "Change Name", -> {change_name}
-        menu.choice "Update Password", -> {change_password}
-        menu.choice "Edit Bio", -> {change_bio}
-        menu.choice "Delete Account ❌".colorize(:red), -> {delete_account}
-        menu.choice "Quit", -> {exit}
-
-
-        ascii = <<-ASCII
-
-
-      )  (
-        (   ) )
-         ) ( (
-       _______)_
-    .-'---------|  
-   ( C|/\/\/\/\/|
-    '-./\/\/\/\/|
-      '_________'
-       '-------'
-
-       ASCII
-       puts ascii
-
-
+    def self.handle_new_user
+        puts "What is your name?"
+        name = gets.chomp
+            while name == "" do
+                system "clear"
+                puts "You must enter a name!".colorize(:red)
+                puts "What is your name?"
+                name = gets.chomp
+            end
+        password = @@prompt.mask("Enter a password:")
+            while password == nil do
+                system "clear"
+                puts "You must enter a password!".colorize(:red)
+                password = @@prompt.mask("Enter a password:")
+            end
+        Borrower.create(name: name, password: password, bio: "Insert bio here")
+        
     end
-end
+
+    def self.handle_returning_user
+        puts "Welcome back! What is your name?"
+        name = gets.chomp
+            while name == "" do
+                system "clear"
+                puts "You must enter a name!".colorize(:red)
+                puts "What is your name?"
+                name = gets.chomp
+            end
+        password = @@prompt.mask("Enter your password:")
+            while password == nil do
+                system "clear"
+                puts "You must enter a password!".colorize(:red)
+                password = @@prompt.mask("Enter a password:")
+            end
+        Borrower.find_by(name: name, password: password)
+    end
+
+
+    def main_menu
+        self.reload
+        system "clear"
+        puts "Welcome, " + self.name.titleize + "!"
+        @@prompt.select("What would you like to do today?".colorize(:cyan)) do |menu|
+            menu.choice "See All Books", -> {display_all_books}
+            menu.choice "See Available Books", -> {display_available_books}
+            menu.choice "See My Borrowed Books", -> {display_my_books}
+            menu.choice "Borrow a Book", -> {borrow_book}
+            menu.choice "Return A Book", -> {return_book}
+            menu.choice "Use as Lender", -> {become_lender}
+            menu.choice "Change Name", -> {change_name}
+            menu.choice "Update Password", -> {change_password}
+            menu.choice "Edit Bio", -> {change_bio}
+            menu.choice "Delete Account ❌".colorize(:red), -> {delete_account}
+            menu.choice "Quit", -> {exit}
+    
+    
+            ascii = <<-ASCII
+          )  (
+            (   ) )
+             ) ( (
+           _______)_
+        .-'---------|  
+       ( C|/\/\/\/\/|
+        '-./\/\/\/\/|
+          '_________'
+           '-------'
+           ASCII
+           puts ascii
+    
+    
+        end
+    end
 
 def display_all_books
     @all_books = Book.pluck(:title)
     @clean_books = @all_books.uniq
 
-if Lender.find_by(name: self.name) != nil
-    my_books = Lender.find_by(name: self.name).books.pluck(:title)
-    my_books.each { |title| @clean_books.delete(title) }
+    if Lender.find_by(name: self.name) != nil
+        my_books = Lender.find_by(name: self.name).books.pluck(:title)
+        my_books.each { |title| @clean_books.delete(title) }
+    end
 
-end
-    
     selected_book = @@prompt.select("Books".colorize(:light_cyan).underline, @clean_books)  
     chosen_book = Book.find_by(title: selected_book)
     puts chosen_book.description
     
     sleep 2
 
-    @@prompt.select ("Would you like to see which books are available or return to the main menu?") do |menu|
+    @@prompt.select ("What would you like to do now?") do |menu|
         menu.choice "Back to All Books", -> {display_all_books}
         menu.choice "See Available Books", -> {display_available_books}
         menu.choice "Return to the Main Menu", -> {main_menu}
@@ -110,17 +107,12 @@ def display_available_books
     @available_books_list = available_books.map do |book|
         book.title
     end
-
-    if Lender.find_by(name: self.name) != nil
-        my_books = Lender.find_by(name: self.name).books.pluck(:title)
-        my_books.each { |title| @available_books_list.delete(title) }
-    end 
     
-    selected_book = @@prompt.select("Books".colorize(:light_cyan).underline, @available_books_list) 
+    selected_book = @@prompt.select("Books", @available_books_list) 
     chosen_book = Book.find_by(title: selected_book)
     puts chosen_book.description
     
-    sleep 2
+    sleep 4
     @@prompt.select ("What would you like to do now?") do |menu|
         menu.choice "Borrow This Book", -> {borrow_book_by_title(selected_book) }
         menu.choice "Back to Available Books", -> {display_available_books}
@@ -145,11 +137,11 @@ def display_my_books
         end
     else
         @all_my_books = self.books.pluck(:title)
-    @clean_books = @all_my_books.uniq
+        @clean_books = @all_my_books.uniq
   
-    @selected_book = @@prompt.select("Books", @clean_books) 
-    @chosen_book = Book.find_by(title: @selected_book)
-    puts @chosen_book.description
+        @selected_book = @@prompt.select("Books", @clean_books) 
+        @chosen_book = Book.find_by(title: @selected_book)
+        puts @chosen_book.description
     ascii = <<-ASCII
          __..._   _...__
     _..-"      `Y`      "-._
@@ -161,11 +153,12 @@ def display_my_books
        '`               `'
     
     ASCII
-    puts ascii
-    sleep 3
-    @@prompt.select ("What would you like to do now?") do |menu|
-        menu.choice "Back to My Books", -> {display_my_books}
-        menu.choice "Return to the Main Menu", -> {main_menu}
+        puts ascii
+        sleep 3
+        @@prompt.select ("What would you like to do now?") do |menu|
+            menu.choice "Back to My Books", -> {display_my_books}
+            menu.choice "Return to the Main Menu", -> {main_menu}
+        end
     end
 end
 
@@ -173,7 +166,7 @@ def change_name
     puts "Please enter your new name here:"
     new_name = gets.chomp
     self.update_attribute(:name, new_name)
-    puts "Your name has been updated.".colorize(:green)
+    puts "Your name has been updated."
     sleep 2
     main_menu
 end
@@ -182,7 +175,7 @@ def change_password
     puts "Please enter your new password here:"
     new_password = gets.chomp
     self.update_attribute(:password, new_password)
-    puts "Your password has been updated.".colorize(:green)
+    puts "Your password has been updated."
     sleep 2
     main_menu
 end
@@ -191,7 +184,7 @@ def change_bio
     puts "Please enter your updated bio here:"
     new_bio = gets.chomp
     self.update_attribute(:bio, new_bio)
-    puts "Your bio has been updated.".colorize(:green)
+    puts "Your bio has been updated."
     sleep 2
     main_menu
 end
@@ -199,7 +192,7 @@ end
 
 def borrow_book
     puts "Please enter the title of the book you'd like to check out."
-    selected_book_id = gets.chomp
+    selected_book = gets.chomp
     selected_book_instance = Book.find_by(title: selected_book)
     selected_book_id = selected_book_instance.id
     if Checkout.pluck(:book_id).include?(selected_book_id)
@@ -209,13 +202,11 @@ def borrow_book
             menu.choice "Yes", -> {open_google_if_not_exists(selected_book_title)}
             menu.choice "No, I'll wait"
         end
-
     else
         Checkout.create(borrower_id: self.id, book_id: selected_book_id)
-        puts "Enjoy your book!"
+        puts "Enjoy your book!".colorize(:green)
 
         ascii = <<-ASCII
-
         _   _                           ____                _ _               _ 
         | | | | __ _ _ __  _ __  _   _  |  _ \ ___  __ _  __| (_)_ __   __ _  | |
         | |_| |/ _` | '_ \| '_ \| | | | | |_) / _ \/ _` |/ _` | | '_ \ / _` | | |
@@ -224,11 +215,9 @@ def borrow_book
                     |_|   |_|    |___/                                 |___/     
                
     ASCII
-    puts ascii
-
+    @@prompt.say(ascii, color: :green)
     end
-
-    sleep 4
+    sleep 3
     main_menu
 end
 
@@ -236,7 +225,7 @@ def borrow_book_by_title(book_title)
     selected_book_instance = Book.find_by(title: book_title)
     selected_book_id = selected_book_instance.id
     Checkout.create(borrower_id: self.id, book_id: selected_book_id)
-    puts @@enjoy_your_book
+    puts "Enjoy your book!"
     sleep 3
     main_menu
 end
@@ -247,7 +236,7 @@ def return_book
     selected_book_instance = Book.find_by(title: selected_book)
     selected_book_id = selected_book_instance.id
     Checkout.where(book_id: selected_book_id).destroy_all
-    puts "Thank you for returning your book!".colorize(:green)
+    puts "Thank you for returning your book!"
     sleep 2
     main_menu
 end
@@ -256,9 +245,8 @@ def delete_account
     puts "Delete Account"
     self.destroy
     puts "Your account has been deleted!".colorize(:red)
+    
     ascii = <<-ASCII
-
-
     ,     ,
     (\____/)
      (_oo_)
@@ -269,10 +257,8 @@ def delete_account
  /    /__\
 (\   /____\
 
-
-
-    ASCII
-    puts ascii
+ASCII
+    puts "ascii"
     sleep 2
 end
 
@@ -287,11 +273,7 @@ def become_lender
     end 
   end 
 
-
-
-
-
-private
+  private
 
   def open_google_if_not_exists(title_query)
     response_string = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{title_query}")
@@ -335,6 +317,7 @@ private
      Lender.find_by(name: self.name).id
     end 
   end 
+
 
 
 
